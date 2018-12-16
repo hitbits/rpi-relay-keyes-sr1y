df04e4cef5701af3124bb6b278d1dacbf1c5e7f3
Javier Sánchez Ochando <jsanchez@deskblue.com>
2018-12-16 11:10:10 +0100

    [DEV] Perform initialization and cleaning tasks
    Add event listeners that performs initialization and cleaning tasks
    after 'connect' or 'disconnect' to the board.

--- src/server.js
+++ src/server.js
@@ -4,13 +4,36 @@
 const five  = require('johnny-five')
 const raspi = require('raspi-io')
 
-// create a raspberry pi board instance
+
+// CONSTANTS DECLARATIONS
+// - Declaration of constants used throughout the code
+
+// board
+// - represents the raspberry-pi board itself
 const board = new five.Board({
   io: new raspi()
 })
 
-// create a relay instance
+// relay
+// - represents the single digital relay attached to the raspberry-pi board
 const relay = new five.Relay({
   type: 'NO',
   pin: 'GPIO4'
+})
+
+
+// EVENT LISTENERS
+// - Defines the functions that are listening for the generated events.
+
+// ready
+// - Execute tasks that must take place before the program can operante after
+//   the board instance object has completed any hardware initialization.
+board.on('ready', () => {
+  board.repl.inject({ relay })
+})
+
+// exit
+// - Execute cleanup task before your program is 'disconnected' from the board
+board.on('exit', () => {
+  relay.off();
 })
