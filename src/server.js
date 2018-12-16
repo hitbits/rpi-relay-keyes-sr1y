92d952b06eb16693ed739caed6b457f7762a5963
Javier Sánchez Ochando <jsanchez@deskblue.com>
2018-12-16 10:55:06 +0100

    [DEV] Create an instance of raspberry-pi board

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
 'use strict'
 
-// TODO: add module dependencies
+// johnny-five and raspi-IO module dependencies
+const five  = require('johnny-five')
+const raspi = require('raspi-io')
+
+// create a raspberry pi board instance
+const board = new five.Board({
+  io: new raspi()
+})
